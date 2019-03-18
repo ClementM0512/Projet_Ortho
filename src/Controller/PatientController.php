c3a1d61f8b1f72c5f67546c2e088ed3d729cf1b7
ClementM0512 <34575889+ClementM0512@users.noreply.github.com>
2019-03-18 11:28:35 +0100

    Ajout + modif
    ajout visuel et de la création des bilans.

--- src/Controller/PatientController.php
+++ src/Controller/PatientController.php
@@ -7,7 +7,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\PatientRepository;
+use App\Repository\BilanRepository;
 use App\Form\PatientType;
+use App\Form\BilanType;
 use App\Entity\Patient;
 use App\Entity\Bilan;
 
@@ -16,11 +18,11 @@ class PatientController extends AbstractController
     /**
      * @Route("/patients", name="patients")
      */
-    public function index(PatientRepository $repo)
+    public function indexPatient(PatientRepository $repo)
     {
         $patient = $repo->findAll();
         
-        return $this->render('patient/index.html.twig', [
+        return $this->render('patient/indexPatient.html.twig', [
             'controller_name' => 'PatientController',
             'patient' => $patient
         ]);
@@ -30,7 +32,7 @@ class PatientController extends AbstractController
      * @Route("/newpatient", name="patientCreate")
      * @Route("/patient/{id}/edit", name="PatientEdit")
      */
-    public function _form(Patient $patient = null, Request $request, ObjectManager $manager)
+    public function _formPatient(Patient $patient = null, Request $request, ObjectManager $manager)
     {
         if (!$patient)
         {
@@ -55,6 +57,51 @@ class PatientController extends AbstractController
         return $this->render('patient/newpatient.html.twig', [
             'formPatient' => $form->createView(),
             'editMode' => $patient->getId() !== null    //si on est en mode édition true/false
+        ]);
+    }
+    
+    /**
+     * @Route("/bilans", name="bilans")
+     */
+    public function indexBilan(BilanRepository $repo)
+    {
+        $bilan = $repo->findAll();
+        
+        return $this->render('patient/indexBilan.html.twig', [
+            'controller_name' => 'BilanController',
+            'bilan' => $bilan
+        ]);
+    }
+    
+    /**
+     * @Route("/newbilan", name="bilanCreate")
+     * @Route("/bilan/{id}/edit", name="bilanEdit")
+     */
+    public function _formBilan(Bilan $bilan = null, Request $request, ObjectManager $manager)
+    {
+        if (!$bilan)
+        {
+            $bilan = new Bilan();
+        }
+        
+        $form = $this->createForm(BilanType::class, $bilan); //constructeur form article
+        
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            //             if (!$patient->getId()){
+            //                 $patient->setCreateAt(new \DateTime());
+            //             }
+            
+            $manager->persist($bilan);
+            $manager->flush();
+            
+            return $this->redirectToRoute('bilans', ['id' => $bilan->getId()]);
+        }
+        
+        return $this->render('patient/newbilan.html.twig', [
+            'formBilan' => $form->createView(),
+            'editMode' => $bilan->getId() !== null    //si on est en mode édition true/false
         ]);
     }
 }
