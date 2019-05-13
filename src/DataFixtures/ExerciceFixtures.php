a93dacb8f2416413a9b3b2e7f05274b3a4d72667
Thomas Canal <thomas.canal@lycee-bourdelle.fr>
2019-05-13 12:47:27 +0200

    je debloque tout le monde

--- src/DataFixtures/ExerciceFixtures.php
+++ src/DataFixtures/ExerciceFixtures.php
@@ -18,49 +18,56 @@ class ExerciceFixtures extends Fixture
         $exercice->setName('Chronomots')
                  ->setImage('image/chronomots.png')
                  ->setLien('chronomots')
-                 ->setDateCreation($faker->dateTime);
+                 ->setDateCreation($faker->dateTime)
+                 ->setDescription("d");
         $manager->persist($exercice);
         
         $exercice = new Exercice();
         $exercice->setName('lestraits')
             ->setImage('image/lestraits.png')
             ->setLien('lestraits')
-            ->setDateCreation($faker->dateTime);
+            ->setDateCreation($faker->dateTime)
+            ->setDescription("d");
         $manager->persist($exercice);
         
         $exercice = new Exercice();
         $exercice->setName('duction')
             ->setImage('image/duction.png')
             ->setLien('duction')
-            ->setDateCreation($faker->dateTime);
+            ->setDateCreation($faker->dateTime)
+            ->setDescription("d");
         $manager->persist($exercice);
         
         $exercice = new Exercice();
         $exercice->setName('lancaster')
             ->setImage('image/lancaster.png')
             ->setLien('lancaster')
-            ->setDateCreation($faker->dateTime);
+            ->setDateCreation($faker->dateTime)
+            ->setDescription("d");
         $manager->persist($exercice);
         
         $exercice = new Exercice();
         $exercice->setName('cartememoire')
         ->setImage('image/cartememoire.png')
         ->setLien('cartememoire')
-        ->setDateCreation($faker->dateTime);
+        ->setDateCreation($faker->dateTime)
+        ->setDescription("d");
         $manager->persist($exercice);
         
         $exercice = new Exercice();
         $exercice->setName('poursuite')
         ->setImage('image/poursuite.png')
         ->setLien('poursuite')
-        ->setDateCreation($faker->dateTime);
+        ->setDateCreation($faker->dateTime)
+        ->setDescription("d");
         $manager->persist($exercice);
         
         $exercice = new Exercice();
         $exercice->setName('Mots-Outil')
         ->setImage('image/motsoutil.png')
         ->setLien('motsoutil')
-        ->setDateCreation($faker->dateTime);
+        ->setDateCreation($faker->dateTime)
+        ->setDescription("d");
         $manager->persist($exercice);
         $manager->flush();
     }
