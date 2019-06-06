cdc8cd824a16db53f2c2aefd603311eb2a0a4975
Thomas Canal <thomas.canal@lycee-bourdelle.fr>
2019-06-06 09:30:05 +0200

    J'ai debugé tt seulc
    c vrai

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,4 +1,4 @@
-﻿<?php 
+<?php
 namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -27,7 +27,6 @@ use App\Entity\Bilan01;
 class MainController extends AbstractController
 {
     /**
-     *
      * @Route("/Patient/{idPatient}/exercices", name="listeExosPatient")
      * @Route("/exercices", name="listeExos")
      * @IsGranted("ROLE_USER")
@@ -186,167 +185,168 @@ class MainController extends AbstractController
         //             $idUser = $this->getUser()->getid();
         //         }
         //         else
-            //         {
-            //             $idUser=0;
+        //         {
+        //             $idUser=0;
             //         }
-        $idExercice = $repoExercice->findOneBy(['name' => 'lettres']);
-        return $this->render('main/lettres.html.twig', [
-            'controller_name' => 'MainController',
-            //             'idPatient' => $idPatient,
-        //             'idUser' => $idUser,
-        //             'idExercice'=> $idExercice->getId(),
-        ]);
-    }
-    
-    /**
-     *
-     * @Route("/exercices/duction", name="duction")
-     * @Route("/Patient/{idPatient}/exercices/duction", name="ductionAP")
-     */
-    public function duction(Patient $patient = null, ExerciceRepository $repoExercice)
-    {
-        if(isset($_GET['idPatient'])){
-            $idPatient = $_GET['idPatient'];
-        } else {
-            $idPatient = 0;
+            $idExercice = $repoExercice->findOneBy(['name' => 'lettres']);
+            return $this->render('main/lettres.html.twig', [
+                'controller_name' => 'MainController',
+                //             'idPatient' => $idPatient,
+                //             'idUser' => $idUser,
+                //             'idExercice'=> $idExercice->getId(),
+            ]);
         }
-        if ($this->getUser()) {
-            $idUser = $this->getUser()->getid();
-        }
-        else
+        
+        /**
+         *
+         * @Route("/exercices/duction", name="duction")
+         * @Route("/Patient/{idPatient}/exercices/duction", name="ductionAP")
+         */
+        public function duction(Patient $patient = null, ExerciceRepository $repoExercice)
         {
-            $idUser=0;
+            if(isset($_GET['idPatient'])){
+                $idPatient = $_GET['idPatient'];
+            } else {
+                $idPatient = 0;
+            }
+            if ($this->getUser()) {
+                $idUser = $this->getUser()->getid();
+            }
+            else
+            {
+                $idUser=0;
+            }
+            $idExercice = $repoExercice->findOneBy(['name' => 'duction']);
+            return $this->render('main/duction.html.twig', [
+                'controller_name' => 'MainController',
+                'idPatient' => $idPatient,
+                'idUser' => $idUser,
+                'idExercice'=> $idExercice->getId(),
+                'bool' => false,
+            ]);
         }
-        $idExercice = $repoExercice->findOneBy(['name' => 'duction']);
-        return $this->render('main/duction.html.twig', [
-            'controller_name' => 'MainController',
-            'idPatient' => $idPatient,
-            'idUser' => $idUser,
-            'idExercice'=> $idExercice->getId(),
-            'bool' => false,
-        ]);
-    }
-    /**
-     *
-     * @Route("/exercices/cartememoire", name="cartememoire")
-     * @Route("/Patient/{idPatient}/exercices/cartememoire", name="cartememoireAP")
-     * @IsGranted("ROLE_USER")
-     */
-    public function cartememoire(Patient $patient = null ,ExerciceRepository $repoExercice)
-    {
-        if(isset($_GET['idPatient'])){
-            $idPatient = $_GET['idPatient'];
-        } else {
-            $idPatient = 0;
-        }
-        if ($this->getUser()) {
-            $idUser = $this->getUser()->getid();
-        }
-        else
+        /**
+         *
+         * @Route("/exercices/cartememoire", name="cartememoire")
+         * @Route("/Patient/{idPatient}/exercices/cartememoire", name="cartememoireAP")
+         * @IsGranted("ROLE_USER")
+         */
+        public function cartememoire(Patient $patient = null ,ExerciceRepository $repoExercice)
         {
-            $idUser=0;
+            if(isset($_GET['idPatient'])){
+                $idPatient = $_GET['idPatient'];
+            } else {
+                $idPatient = 0;
+            }
+            if ($this->getUser()) {
+                $idUser = $this->getUser()->getid();
+            }
+            else
+            {
+                $idUser=0;
+            }
+            $idExercice = $repoExercice->findOneBy(['name' => 'cartememoire']);
+            return $this->render('main/cartememoire.html.twig', [
+                'controller_name' => 'MainController',
+                'idPatient' => $idPatient,
+                'idUser' => $idUser,
+                'idExercice'=> $idExercice->getId(),
+                'bool' => false,
+            ]);
         }
-        $idExercice = $repoExercice->findOneBy(['name' => 'cartememoire']);
-        return $this->render('main/cartememoire.html.twig', [
-            'controller_name' => 'MainController',
-            'idPatient' => $idPatient,
-            'idUser' => $idUser,
-            'idExercice'=> $idExercice->getId(),
-            'bool' => false,
-        ]);
-    }
-    
-    /**
-     *
-     * @Route("/exercices/poursuite", name="poursuite")
-     * @Route("/Patient/{idPatient}/exercices/poursuite", name="poursuiteAP")
-     * @IsGranted("ROLE_USER")
-     */
-    public function Poursuite(Patient $patient = null, HistoireRepository $repo, ExerciceRepository $repoExercice)
-    {
-        if (isset($_GET['idPatient'])) {
-            $idPatient = $_GET['idPatient'];
-        } else {
-            $idPatient = 0;
-        }
-        if ($this->getUser()) {
-            $idUser = $this->getUser()->getid();
-        }
-        else
+        
+        /**
+         *
+         * @Route("/exercices/poursuite", name="poursuite")
+         * @Route("/Patient/{idPatient}/exercices/poursuite", name="poursuiteAP")
+         * @IsGranted("ROLE_USER")
+         */
+        public function Poursuite(Patient $patient = null, HistoireRepository $repo, ExerciceRepository $repoExercice)
         {
-            $idUser=0;
+            if (isset($_GET['idPatient'])) {
+                $idPatient = $_GET['idPatient'];
+            } else {
+                $idPatient = 0;
+            }
+            if ($this->getUser()) {
+                $idUser = $this->getUser()->getid();
+            }
+            else
+            {
+                $idUser=0;
+            }
+            $histoires = $repo->findAll();
+            $idExercice = $repoExercice->findOneBy(['name' => 'poursuite']);
+            return $this->render('main/poursuite.html.twig', [
+                'controller_name' => 'MainController',
+                'histoires' => $histoires,
+                'idPatient' => $idPatient,
+                'idUser' => $idUser,
+                'idExercice'=> $idExercice->getId(),
+                'bool' => false,
+            ]);
         }
-        $histoires = $repo->findAll();
-        $idExercice = $repoExercice->findOneBy(['name' => 'poursuite']);
-        return $this->render('main/poursuite.html.twig', [
-            'controller_name' => 'MainController',
-            'histoires' => $histoires,
-            'idPatient' => $idPatient,
-            'idUser' => $idUser,
-            'idExercice'=> $idExercice->getId(),
-            'bool' => false,
-        ]);
-    }
-    
-    /**
-     *
-     * @Route("/receptionajax", name="receptionajax")
-     */
-    public function receptionajax(HistoireRepository $repo)
-    {
-        $histoires = $repo->findAll();
-        for ($i = 0; $i < count($histoires); $i ++) {
-            $arr[$i] = $histoires[$i]->getTexte();
+        
+        /**
+         *
+         * @Route("/receptionajax", name="receptionajax")
+         */
+        public function receptionajax(HistoireRepository $repo)
+        {
+            $histoires = $repo->findAll();
+            for ($i = 0; $i < count($histoires); $i ++) {
+                $arr[$i] = $histoires[$i]->getTexte();
+            }
+            echo (json_encode($arr));
+            return $this->render('main/sendarticle.html.twig');
         }
-        echo (json_encode($arr));
-        return $this->render('main/sendarticle.html.twig');
-    }
-    /**
-     *
-     * @Route("/envoiajax", name="envoiajax")
-     */
-    public function envoieAjax(ObjectManager $manager, ExerciceRepository $repoExo, PatientRepository $repoPatient, UserRepository $repoUser, Bilan01Repository $repoBilan)
-    {
-        $exercice = $repoExo->find((int)$_GET['exercice']);
-        $bilan = $repoBilan->find((int)$_GET['bilan']);
-        $user = $repoUser->find((int)$_GET['user']);
-        $patient = $repoPatient->find((int)$_GET['patient']);
-        
-        $resultat = new Resultat();
-        $resultat->setIdExercice($exercice)
-        ->setIdPatient($patient)
-        ->setIdUser($user)
-        ->setIdBilan($bilan)
-        ->setScore($_GET['score']);
-        
-        //         echo($_GET['exercice']);
-        
-        $manager->persist($resultat);
-        $manager->flush();
-        return $this->render('main/sendarticle.html.twig');
-        
-    }
-    /**
-     *
-     * @Route("/nouvellehistoire", name="HistoireCreation")
-     * @IsGranted("ROLE_USER")
-     */
-    public function createHistoire(Request $request, ObjectManager $manager)
-    {
-        $histoire = new Histoire();
-        $form = $this->createFormBuilder($histoire)
-        ->add('name')
-        ->add('texte')
-        ->add('save', SubmitType::class)
-        ->getForm();
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $manager->persist($histoire);
+        /**
+         *
+         * @Route("/envoiajax", name="envoiajax")
+         */
+        public function envoieAjax(ObjectManager $manager, ExerciceRepository $repoExo, PatientRepository $repoPatient, UserRepository $repoUser, Bilan01Repository $repoBilan)
+        {
+            $exercice = $repoExo->find((int)$_GET['exercice']);
+            $bilan = $repoBilan->find((int)$_GET['bilan']);
+            $user = $repoUser->find((int)$_GET['user']);
+            $patient = $repoPatient->find((int)$_GET['patient']);
+            
+            $resultat = new Resultat();
+            $resultat->setIdExercice($exercice)
+            ->setIdPatient($patient)
+            ->setIdUser($user)
+            ->setIdBilan($bilan)
+            ->setScore($_GET['score']);
+            
+            //         echo($_GET['exercice']);
+            
+            $manager->persist($resultat);
             $manager->flush();
-            return $this->redirectToRoute('listeExos');
+            return $this->render('main/sendarticle.html.twig');
+            
         }
-        return $this->render('main/nouvellehistoire.html.twig', [
-            'formHistoire' => $form->createView()
-        ]);
+        /**
+         *
+         * @Route("/nouvellehistoire", name="HistoireCreation")
+         * @IsGranted("ROLE_USER")
+         */
+        public function createHistoire(Request $request, ObjectManager $manager)
+        {
+            $histoire = new Histoire();
+            $form = $this->createFormBuilder($histoire)
+            ->add('name')
+            ->add('texte')
+            ->add('save', SubmitType::class)
+            ->getForm();
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $manager->persist($histoire);
+                $manager->flush();
+                return $this->redirectToRoute('listeExos');
+            }
+            return $this->render('main/nouvellehistoire.html.twig', [
+                'formHistoire' => $form->createView()
+            ]);
+        }
     }
-}
+    
